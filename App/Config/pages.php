88616574a3d53ff9f601e3ae68a1be61cb5bc91c
1ndoryu <andoryyu@gmail.com>
2025-12-09 14:58:04 -0400

    feat: añade estilos iniciales para el tema, incluyendo variables de color, calendario y utilidades.

--- App/Config/pages.php
+++ App/Config/pages.php
@@ -6,7 +6,6 @@ use Glory\Core\GloryFeatures;
 PageManager::setDefaultContentMode('code');
 
 PageManager::define('home', 'home');
-PageManager::define('contructor', 'contructor');
 
 if (GloryFeatures::isActive('task') !== false) {
     PageManager::define('task', 'task');
