088f5a6b9319c3f74fd14a823ce94f43724bb4b1
1ndoryu <andoryyu@gmail.com>
2026-01-16 19:07:04 -0400

    Integrar favicon SVG en header.php

--- header.php
+++ header.php
@@ -23,6 +23,7 @@ $usuarioId = get_current_user_id();
     </style>
     <!-- Assets encolados por AssetManager: GSAP y Highlight.js (controlados por features) -->
     <link rel="profile" href="https://gmpg.org/xfn/11">
+    <link rel="icon" type="image/svg+xml" href="<?php echo get_template_directory_uri(); ?>/Glory/assets/images/favicon.svg">
     <?php wp_head(); ?>
 </head>
 
@@ -33,8 +34,8 @@ $usuarioId = get_current_user_id();
 
 <body>
     <div id="loadingBar"></div>
-    
-    
+
+
     <?php
 
     ?>
